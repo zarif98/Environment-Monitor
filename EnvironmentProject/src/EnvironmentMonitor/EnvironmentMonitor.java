1ab2c17e4773da8025a23d476526c074ea2dd15a
zarif98 <zarifurzarif@gmail.com>
2019-04-25 18:42:42 -0400

    automatic

--- EnvironmentProject/src/EnvironmentMonitor/EnvironmentMonitor.java
+++ EnvironmentProject/src/EnvironmentMonitor/EnvironmentMonitor.java
@@ -14,7 +14,13 @@ public class EnvironmentMonitor {
 	public ArrayList<Environment> getEnvironment() {
 		return environment;
 	}
-	public void createJobs() {
+	public void createAutomaticJobs() {
+		environments.addJobs(new Jobs("Pick Up Trash", "Clean the area near the river basin", DesiredTask.pickUpTrash));
+		environments.addJobs(new Jobs("Rake Leaves", "Clean the area near the trees ", DesiredTask.rakeLeaves));
+		environments.addJobs(new Jobs("Maintain Paths", "Clean the paths near the forest", DesiredTask.maintain_Paths));
+	}
+	public void createJobs(String jobName, String description, String skills) {
+		environments.addJobs(new Jobs(jobName, description, DesiredTask.valueOf(skills)));
 	}
 
 	public void setEnvironment(ArrayList<Environment> environment) {
