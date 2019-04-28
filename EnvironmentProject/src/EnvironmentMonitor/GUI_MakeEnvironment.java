779c0bb4c0c48d8e84ef9506d2c7a85451e9a083
zarif98 <zarifurzarif@gmail.com>
2019-04-27 23:23:53 -0400

    Universal Error popup and updates to Make Environment

--- EnvironmentProject/src/EnvironmentMonitor/GUI_MakeEnvironment.java
+++ EnvironmentProject/src/EnvironmentMonitor/GUI_MakeEnvironment.java
@@ -5,9 +5,11 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 public class GUI_MakeEnvironment implements isDialog {
@@ -24,11 +26,29 @@ public class GUI_MakeEnvironment implements isDialog {
 		TextField environmentName = new TextField();
 		environmentName.setPromptText("Environment Name");
 		
-		CheckBox LandEnvironmentBox = new CheckBox();
+		CheckBox landEnvironmentBox = new CheckBox("Land Environment");
+		CheckBox waterEnvironmentBox = new CheckBox("Water Environment");
+		Popup popup = new Popup();
+		popup.setX(300);
+		popup.setY(200);
 		
-		
+		Button submit = new Button("Submit");
+		submit.setOnAction(e ->{
+			if(landEnvironmentBox.isSelected()) {
+				LandEnvironment landEnvironment = new LandEnvironment(environmentName.getText());
+			}
+			else if(waterEnvironmentBox.isSelected()) {
+				WaterEnvironment waterEnvironment = new WaterEnvironment(environmentName.getText());
+			}
+			else if(landEnvironmentBox.isSelected() && waterEnvironmentBox.isSelected()) {
+				WaterAndLandEnvironment waterAndLandEnvironment = new WaterAndLandEnvironment(environmentName.getText());
+			}
+			else{
+				GUI_UniversalErrorPopUp.showStage(GUI_MakeEnvironment.class);
+			}
+		});
 		VBox pane = new VBox(20);
-		pane.getChildren().addAll();
+		pane.getChildren().addAll(environmentName, landEnvironmentBox, waterEnvironmentBox, submit);
 		pane.setAlignment(Pos.CENTER);
 		pane.setStyle("-fx-background-color: AntiqueWhite;");
 		pane.setPadding(new Insets(20,20,20,20));
@@ -37,5 +57,4 @@ public class GUI_MakeEnvironment implements isDialog {
 		stage.setScene(scene);
 		stage.showAndWait();
 	}
-
 }
