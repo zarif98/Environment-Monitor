35c478d6aaae9d6719f629c40af2bbe25710a6a2
mkane <mkane@DESKTOP-UH3VPCP.rowanads.rowan.edu>
2019-04-29 19:50:01 -0400

    job view

---
+++ EnvironmentProject/src/EnvironmentMonitor/GUI_JobView.java
@@ -0,0 +1,81 @@
+package EnvironmentMonitor;
+
+import java.util.Arrays;
+
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeTableColumn.CellDataFeatures;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
+public class GUI_JobView implements isDialog{
+	/**
+	 * Code for dialog in which 
+	 */
+	public static void dialog(Environment environment) {
+		Stage stage = new Stage();
+		stage.initModality(Modality.APPLICATION_MODAL);
+		stage.setTitle("Showing all jobs for selected Job");
+		stage.setMinWidth(500);
+
+		TableView<Job> jobView = new TableView<Job>(); 
+
+
+		TableColumn<Job, String> colName =
+				new TableColumn<Job, String>("Job Name");
+		colName.setMinWidth(180);
+		colName.setCellValueFactory(
+				new PropertyValueFactory<Job, String>("jobName"));
+		
+		TableColumn<Job, String> colDesc =
+				new TableColumn<Job, String>("Job Description");
+		colDesc.setMinWidth(80);
+		colDesc.setCellValueFactory(
+				new PropertyValueFactory<Job, String>("jobDescription"));
+
+		TableColumn<Job, String> colTask =
+				new TableColumn<Job, String>("Desired Task");
+		colDesc.setMinWidth(80);
+		colDesc.setCellValueFactory(
+				new PropertyValueFactory<Job, String>("desiredTask"));
+
+
+
+		jobView.getColumns().setAll(colName, colDesc, colTask);
+
+		for(Job j: environment.getJobs()) {
+			jobView.getItems().add(j);
+		}
+
+		VBox pane = new VBox(20);
+		pane.getChildren().addAll(jobView);
+		pane.setAlignment(Pos.CENTER);
+		pane.setStyle("-fx-background-color: AntiqueWhite;");
+		pane.setPadding(new Insets(20,20,20,20));
+
+		Scene scene = new Scene(pane);
+		stage.setScene(scene);
+		stage.showAndWait();
+	}
+}
