172e0593956558078565c2cae95a58819179550a
Luke LaValva <lulavalva@gmail.com>
2019-04-29 20:21:36 -0400

    Added some JavaDoc

--- EnvironmentProject/src/EnvironmentMonitor/DesiredTask.java
+++ EnvironmentProject/src/EnvironmentMonitor/DesiredTask.java
@@ -8,12 +8,17 @@ public enum DesiredTask {
 	
 	private final String desiredTask;
 	
+	/**
+	 * Constructor for each item, stores string for name
+	 * @param desiredTask
+	 */
 	DesiredTask(String desiredTask){
 		this.desiredTask = desiredTask;
 	}
-	public String getDesiredTask() {
-		return this.desiredTask;
-	}
+	
+	/**
+	 * toString, returns desired task
+	 */
 	public String toString() {
 		return this.desiredTask;
 	}
--- EnvironmentProject/src/EnvironmentMonitor/Environment.java
+++ EnvironmentProject/src/EnvironmentMonitor/Environment.java
@@ -15,22 +15,45 @@ public abstract class Environment implements Serializable{
 	protected double longitude;
 	protected double latitude;
 	
+	/**
+	 * Constructor for environment, sets instance variables
+	 * @param environmentName
+	 * @param latitude
+	 * @param longitude
+	 */
 	public Environment(String environmentName, double latitude
 			, double longitude) {
 		this.environmentName = environmentName;
 		this.longitude = longitude;
 		this.latitude = latitude;
 	}
-	public void addJob(Job job) {
-		jobs.add(job);
+	
+	/**
+	 * Adds a job to an environment
+	 * @param job
+	 * @return true if job was successfully added
+	 */
+	public boolean addJob(Job job) {
+		return jobs.add(job);
 	}
-	public void addVolunteers(Volunteer volunteer) {
-		volunteers.add(volunteer);
+	
+	/**
+	 * Add volunteer to list
+	 * @param volunteer
+	 * @return true if successful
+	 */
+	public boolean addVolunteers(Volunteer volunteer) {
+		return volunteers.add(volunteer);
 	}
+	
+	/**
+	 * Find distance from Rowan
+	 * @return distance in miles
+	 */
 	public double getDistanceTo() {
 		double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
-        double lat1 = Math.toRadians(39.70701);
-        double lon1 = Math.toRadians(-75.1207);
+        double lat1 = Math.toRadians(39.70701); //latitude of Rowan
+        double lon1 = Math.toRadians(-75.1207); //longitude of Rowan
         double lat2 = Math.toRadians(this.latitude);
         double lon2 = Math.toRadians(this.longitude);
 
@@ -44,14 +67,27 @@ public abstract class Environment implements Serializable{
         return statuteMiles;
 	}
 	
+	/**
+	 * toString for environment
+	 * @return String with the name and distance from Rowan
+	 */
 	public String toString() {
 		return environmentName + " (" + latitude + ", " + longitude + ")" +
 				" is " + getDistanceTo() + "km away.";
 	}
 	
+	/**
+	 * Getter for environment name
+	 * @return environmentName
+	 */
 	public String getEnvironmentName() {
 		return environmentName;
-	}	
+	}
+	
+	/**
+	 * Getter for list of jobs
+	 * @return
+	 */
 	public ArrayList<Job> getJobs(){
 		return jobs;
 	}
