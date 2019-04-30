5aef3965e2a821ea82928f216f09a0f3d18b63da
Luke LaValva <lulavalva@gmail.com>
2019-04-29 20:34:23 -0400

    javadoc for JobList

--- EnvironmentProject/src/EnvironmentMonitor/JobList.java
+++ EnvironmentProject/src/EnvironmentMonitor/JobList.java
@@ -6,6 +6,9 @@ public class JobList extends ObjectList {
 
 	private static final String JOBS_FILE = "SavedInfo/jobs.ser";
 	
+	/**
+	 * Constructor, calls superclass using Job arrayList & file
+	 */
 	public JobList() {
 		super(new HashSet<Job>(), JOBS_FILE);
 	}
