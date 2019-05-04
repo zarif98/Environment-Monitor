9bc4be39efa892e0c8fbf95e51d50c0033e4849b
zarif98 <zarifurzarif@gmail.com>
2019-05-04 18:04:50 -0400

    Submit button fixed

--- EnvironmentProject/src/EnvironmentMonitor/GUI_Submit.java
+++ EnvironmentProject/src/EnvironmentMonitor/GUI_Submit.java
@@ -3,11 +3,19 @@ package EnvironmentMonitor;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 public class GUI_Submit implements isDialog {
+	
+	static TableView<Job> userjobView = new TableView<Job>();
 
 	/**
 	 * Code for dialog in which 
@@ -18,10 +26,44 @@ public class GUI_Submit implements isDialog {
 		stage.setTitle("");
 		stage.setMinWidth(0);
 		
+
+		TableColumn<Job, String> colDesc =
+				new TableColumn<Job, String>("Log");
+		colDesc.setMinWidth(200);
+		colDesc.setCellValueFactory(
+				new PropertyValueFactory<Job, String>("jobName"));
+
+		TableColumn<Job, String> colRemove = 
+				new TableColumn<Job, String>("Remove jobs?");
+		Callback<TableColumn<Job, String>, TableCell<Job, String>> cellFactory = new Callback<TableColumn<Job, String>, TableCell<Job, String>>() {
+			public TableCell<Job, String> call(TableColumn<Job, String> param) {
+				final TableCell<Job, String> cell = new TableCell<Job, String>() {
+					private final Button button = new Button();
+
+					public void updateItem(String crn, boolean empty) {
+						if(empty) {
+							setGraphic(null);
+						} else {
+							Job currentJob = getTableView().getItems().get(getIndex());
+							
+							button.setText("Remove");
+							button.setAlignment(Pos.BASELINE_CENTER);
+							button.setMaxWidth(Double.MAX_VALUE);
+							setGraphic(button);
+						}
+					}
+				};
+				return cell;
+			}
+		};
+		
+		colRemove.setCellFactory(cellFactory);
+		
+		userjobView.getColumns().setAll(colDesc, colRemove);
 		
 		
 		VBox pane = new VBox(20);
-		pane.getChildren().addAll();
+		pane.getChildren().addAll(userjobView);
 		pane.setAlignment(Pos.CENTER);
 		pane.setStyle("-fx-background-color: AntiqueWhite;");
 		pane.setPadding(new Insets(20,20,20,20));
