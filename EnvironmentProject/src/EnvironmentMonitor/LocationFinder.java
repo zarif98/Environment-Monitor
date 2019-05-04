b30c9797543b68a555fe9b6fda3388fb2e05c662
Luke LaValva <lulavalva@gmail.com>
2019-05-04 18:58:13 -0400

    Created Location FInder class

---
+++ EnvironmentProject/src/EnvironmentMonitor/LocationFinder.java
@@ -0,0 +1,11 @@
+package EnvironmentMonitor;
+
+public class LocationFinder {
+
+	private static String url = "";
+	
+	public static double[] findLocation(String info) {
+		return new double[2];
+	}
+
+}
